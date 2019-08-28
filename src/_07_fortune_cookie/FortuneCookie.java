496a524feda0d2a7fe248b2c0335ef70a09a1fd8
League Student <league@iMac-37.attlocal.net>
2019-08-27 18:23:55 -0700

    8/27/2019
    :smile:

--- src/_07_fortune_cookie/FortuneCookie.java
+++ src/_07_fortune_cookie/FortuneCookie.java
@@ -1,5 +1,46 @@
 package _07_fortune_cookie;
 
-public class FortuneCookie {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class FortuneCookie implements ActionListener {
+	       
+		public void showButton() {
+			JFrame frame = new JFrame();
+			frame.setVisible(true);
+			JButton button = new JButton();
+			frame.add(button);
+			frame.pack();
+			button.addActionListener(this);
+			
+			Random rando = new Random();
+			int i = rando.nextInt(5);
+			if(i == 0) {
+				JOptionPane.showMessageDialog(null, "you're gonna die");
+			} else if(i == 1) {
+				JOptionPane.showMessageDialog(null, "you're gonna trip and fall");
+			} else if(i == 2) {
+				JOptionPane.showMessageDialog(null, "on April 6th, 6:29 pm, you will suddenly experience a series of extremely painful heart attacks, and will be immediately rushed to the ER, only to die of unexplained circumstances, even though you have already recovered from the heart attacks");
+			} else if(i == 3) {
+				JOptionPane.showMessageDialog(null, "what fortune");
+			} else if(i == 4) {
+				JOptionPane.showMessageDialog(null,  "bruh");
+			} else if(i == 5) {
+				JOptionPane.showMessageDialog(null, "lmao");
+			}
+	    }
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			JOptionPane.showMessageDialog(null, "woohoo");
+		}
+	
+		
+		
 }
---
+++ src/_08_jack_in_the_box/JackInTheBoxRunner.java
@@ -0,0 +1,11 @@
+package _08_jack_in_the_box;
+
+import _08_jack_in_the_box.JackInTheBox;
+
+public class JackInTheBoxRunner {
+
+	public static void main(String[] args) {
+		new JackInTheBox().showButton();
+	}		
+
+}
